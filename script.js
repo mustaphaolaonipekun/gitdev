87c82d8e59dcdc48192994bcd4dc8431929c4040
mustydev <olaonipekunmustapha47@gmail.com>
2023-07-08 15:37:06 +0100

    just added new js file

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Musty is perfect with git and github");
+let main = document.querySelector(". main");
+if (main == main){
+    main.innerHTML = "This is written with a js file"
+}else{
+    main.innerHTML="    This is the main part of the page"
+}
